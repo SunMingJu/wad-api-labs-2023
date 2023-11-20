26074cc9c57ecdb896cd5dd030a941db146a5b06
MingJu Sun <20104599@mail.wit.ie>
2023-11-20 01:34:44 +0000

    Week 9 Lab exercise done

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -21,7 +21,10 @@ router.post('/', (req, res) => {
         description,
         deadline,
         priority,
-        done
+        done,
+        create_at: new Date().toISOString(),
+        updated_at: new Date().toISOString(),
+
     };
     tasksData.tasks.push(newTask);
     res.status(201).json(newTask);
@@ -34,7 +37,7 @@ router.put('/:id', (req, res) => {
     if (taskIndex === -1) {
         return res.status(404).json({ status: 404, message: 'Task not found' });
     }
-    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, id:id };
+    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, updated_at: new Date().toISOString(), id:id };
     tasksData.tasks[taskIndex] = updatedTask;
     res.json(updatedTask);
 });
