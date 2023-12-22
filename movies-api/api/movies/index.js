545c85ab90f51df7a32281db5d48ce768632d24c
MingJu Sun <20104599@mail.wit.ie>
2023-12-22 08:27:47 +0000

    Added Pagination

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -23,9 +23,25 @@ router.get('/:id', asyncHandler(async (req, res) => {
     }
 }));
 
-router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
-    const upcomingMovies = await getUpcomingMovies();
-    res.status(200).json(upcomingMovies);
+router.get('/', asyncHandler(async (req, res) => {
+    let { page = 1, limit = 10 } = req.query; // destructure page and limit and set default values
+    [page, limit] = [+page, +limit]; //trick to convert to numeric (req.query will contain string values)
+
+    // Parallel execution of counting movies and getting movies using movieModel
+    const [total_results, results] = await Promise.all([
+        movieModel.estimatedDocumentCount(),
+        movieModel.find().limit(limit).skip((page - 1) * limit)
+    ]);
+    const total_pages = Math.ceil(total_results / limit); //Calculate total number of pages (= total No Docs/Number of docs per page) 
+
+    //construct return Object and insert into response object
+    const returnObject = {
+        page,
+        total_pages,
+        total_results,
+        results
+    };
+    res.status(200).json(returnObject);
 }));
 
 export default router;
