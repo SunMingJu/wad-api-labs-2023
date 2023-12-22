af92d611cf3093dc2be9d28e8b4d46183e2b6f9f
MingJu Sun <20104599@mail.wit.ie>
2023-12-22 08:25:37 +0000

    Added Auth Middleware

--- movies-api/index.js
+++ movies-api/index.js
@@ -5,7 +5,7 @@ import usersRouter from './api/users';
 import './db';
 import defaultErrHandler from './errHandler';
 import moviesRouter from './api/movies';   //import movies router
-
+import authenticate from './authenticate';
 
 dotenv.config();
 
@@ -17,6 +17,8 @@ app.use(express.json());
 app.use('/api/users', usersRouter);
 app.use(defaultErrHandler);
 app.use('/api/movies', moviesRouter); //ADD THIS BEFORE THE DEFAULT ERROR HANDLER.
+app.use('/api/movies',authenticate,  moviesRouter);
+
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
